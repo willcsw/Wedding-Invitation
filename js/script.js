d73564929bbb21251e6ff170347a62c955e698d5
willcsw <67531201+willcsw@users.noreply.github.com>
2023-02-20 14:29:06 -0800

    test

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ var x = setInterval(function() {
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+        document.getElementById("time").innerHTML = "Bless the married couple for happy life!!";
     }
 }, 1000);
 
